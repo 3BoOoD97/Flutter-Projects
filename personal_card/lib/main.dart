4c8cd9b001dfbbeccabf9202509f3190e3c28476
3BoOoD97 <abodn70@hotmail.com>
2023-06-23 19:03:07 +0200

    Add my Img and my name

--- personal_card/lib/main.dart
+++ personal_card/lib/main.dart
@@ -2,11 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 136, 168, 211),
+        body: SafeArea(
+            child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage('images/me.jpg'),
+            ),
+            Text(
+              'Abdalrhman Dabor',
+              style: TextStyle(
+                fontFamily: 'PatrickHandSC',
+                fontSize: 35.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Software Developer',
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
 }
