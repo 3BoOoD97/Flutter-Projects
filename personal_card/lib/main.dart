06fb8da543886ad3cc3dc035cf60b293acb890ea
3BoOoD97 <abodn70@hotmail.com>
2023-06-24 19:18:32 +0200

    Done

--- personal_card/lib/main.dart
+++ personal_card/lib/main.dart
@@ -11,34 +11,97 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 136, 168, 211),
+        backgroundColor: Colors.cyan[700],
         body: SafeArea(
-            child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50.0,
-              backgroundColor: Colors.white,
-              backgroundImage: AssetImage('images/me.jpg'),
-            ),
-            Text(
-              'Abdalrhman Dabor',
-              style: TextStyle(
-                fontFamily: 'PatrickHandSC',
-                fontSize: 35.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('images/me.jpg'),
               ),
-            ),
-            Text(
-              'Software Developer',
-              style: TextStyle(
-                fontSize: 15.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+              Text(
+                'Abdalrhman Dabor',
+                style: TextStyle(
+                  fontFamily: 'PatrickHandSC',
+                  fontSize: 45.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
-        )),
+              Text(
+                'Software Developer',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 200.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(20),
+                color: Color.fromARGB(255, 1, 1, 1),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    'E-mail: ad222uk@student.lnu.se',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: const Color.fromARGB(255, 6, 6, 6),
+                margin: EdgeInsets.all(20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    'Phone: +46 737566594',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: const Color.fromARGB(255, 6, 6, 6),
+                margin: EdgeInsets.all(20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.web,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    'web: https://lnu.se/',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
