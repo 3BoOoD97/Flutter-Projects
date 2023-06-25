78a2d647f3cc2dd441732c698d50f58acaea31c0
3BoOoD97 <abodn70@hotmail.com>
2023-06-25 18:15:36 +0200

    Start the project and design the app

---
+++ ToDoApp/to_do_app/lib/screens/tasks_screens.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.teal[500],
+        child: Icon(Icons.add),
+      ),
+      backgroundColor: Colors.teal[500],
+      body: Container(
+        padding: const EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(
+            children: [
+              Icon(Icons.playlist_add_check, size: 40.0, color: Colors.white),
+              const SizedBox(width: 10.0),
+              Text(
+                'ToDoList',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            "Tasks",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          ),
+          const SizedBox(height: 30.0),
+          Expanded(
+            child: Container(
+                decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20.0),
+            )),
+          )
+        ]),
+      ),
+    );
+  }
+}
